3db6d14fcdebab6b4e9490dcac1376badbaf2232
Kevin <stradtkt22@gmail.com>
2019-02-23 06:36:03 -0500

    Initial Commit

---
+++ app/src/main/java/com/example/calculator/MainActivity.java
@@ -0,0 +1,26 @@
+package com.example.calculator;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+    private EditText result;
+    private EditText newNumber;
+    private TextView displayOperation;
+
+    private Double operand1 = null;
+    private Double operand2 = null;
+    private String pendingOperation = "=";
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        result = (EditText) findViewById(R.id.result);
+
+    }
+
+}
